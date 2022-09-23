c3ec82067fbe254153c3c5aabcc3af892810f96f
prasathram387 <42970694+prasathram387@users.noreply.github.com>
2022-09-23 20:30:25 +0530

    Employee Management using Spring Boot

---
+++ src/main/java/com/i2i/employeemanagement/controller/JwtUserDetailsService.java
@@ -0,0 +1,11 @@
+package com.i2i.employeemanagement.controller;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+
+
+}
